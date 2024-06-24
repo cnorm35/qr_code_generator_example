2c6cf543db1191594dee3dfb604c5f9eacb08bd3
Cody Norman <wcodynorman@gmail.com>
2024-06-24 13:34:58 -0600

    create full QR code in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+qr_code = QrCode.new(url: "https://www.rubyonrails.org")
+qr_code.logo.attach(io: File.open("app/assets/images/rails_logo.png"), filename: "rails_logo.png", content_type: "image/png")
+qr_code.generate_qr_code!
+qr_code.generate_combined_image!
+
